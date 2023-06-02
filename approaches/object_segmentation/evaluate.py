8cbe2ef5ca08bff52133d4d4454005f59e3a591d
jkoessle <jonathan.koessler@gmx.de>
2023-06-02 18:57:18 +0200

    Update evaluation

--- approaches/object_segmentation/evaluate.py
+++ approaches/object_segmentation/evaluate.py
@@ -1,3 +1,8 @@
+# import logging, os
+
+# logging.disable(logging.WARNING)
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
 import tensorflow as tf
 
 import utils.config as cfg
@@ -9,10 +14,17 @@ def evaluate(model):
     utils.visualize_predictions(path=cfg.EVAL_DATA_DIR,
                                 mode="validation",
                                 model=model)
-
+    
 
 if __name__ == "__main__":
-
-    model = tf.saved_model.load(cfg.TRAINED_MODEL_PATH)
+    # tf.get_logger().setLevel('ERROR')
     
-    evaluate(model)
+    # from absl import logging
+
+    # tf.autograph.set_verbosity(0)
+    model = tf.saved_model.load(cfg.TRAINED_MODEL_PATH)
+    threshold = 0.5
+    
+    utils.get_drift_moments(None, eval_dir=cfg.EVAL_DATA_DIR, model=model)
+    
+    # evaluate(model)
