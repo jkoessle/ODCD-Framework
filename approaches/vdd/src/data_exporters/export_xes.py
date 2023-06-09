2fcbea966d4be536aed1248d682e74beadcf7078
jkoessle <jonathan.koessler@gmx.de>
2023-06-09 19:17:23 +0200

    Delete VDD repo

--- approaches/vdd/src/data_exporters/export_xes.py
+++
@@ -1,5 +0,0 @@
-from pm4py.objects.log.exporter.xes import exporter as xes_exporter
-
-def export_xes_log(log, fileMngm):
-    xes_exporter.apply(log, str(fileMngm.get_path_input_sorted_xes()))
-
--- approaches/vdd/src/scenario_2.py
+++
@@ -1,7 +0,0 @@
-from src.auxiliary.command_line import get_commandline_parameters
-from src.visualize_dfg_with_constraints import visualize_dfg_with_constraints
-
-fileMngm, algoPrmts = get_commandline_parameters()
-
-visualize_dfg_with_constraints(fileMngm)
-
